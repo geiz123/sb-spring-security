5a7d27dcbdc85a5c193153f65ea18bfe863f73f1
geiz123 <geiz123@users.noreply.github.com>
2025-05-02 15:39:42 -0400

    Some spring security stuff

---
+++ src/main/java/com/example/demo/config/DevSecurityConfig.java
@@ -0,0 +1,29 @@
+package com.example.demo.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+/**
+ * Allow all access
+ */
+@Profile("dev")
+@Configuration
+public class DevSecurityConfig {
+
+    Logger logger = LoggerFactory.getLogger(DevSecurityConfig.class);
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        logger.info("{}(((((((((()))))))))){}Allow all access{}(((((((((())))))))))", System.lineSeparator(),
+                System.lineSeparator(), System.lineSeparator());
+
+        http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+        return http.build();
+    }
+}
---
+++ src/main/java/com/example/demo/ctrl/CtrlOne.java
@@ -0,0 +1,14 @@
+package com.example.demo.ctrl;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CtrlOne {
+
+    @GetMapping("/hello/{id}")
+    String one(@PathVariable Long id) {
+        return "hello " + id;
+    }
+}
